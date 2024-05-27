07ab7847fd121c137769b39d71e584fac058f037
Muhammet Talha Seferoğlu <talhaseferoglu34@gmail.com>
2024-05-27 22:58:00 +0300

    internet connection check

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React, { Component } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore} from 'redux'; 
+import { applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import NetInfo from '@react-native-community/netinfo';
 import firebase from 'firebase/compat/app';
 import "firebase/storage";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore} from 'redux'
-import { applyMiddleware } from 'redux';
 import rootReducer from './redux/reducers';
-import {thunk} from 'redux-thunk';
-const store = createStore(rootReducer, applyMiddleware(thunk));
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import LoginScreen from './components/auth/Login';
+import MainScreen from './components/Main';
+import AddScreen from './components/main/Add';
+import SaveScreen from './components/main/Save';
+import AIChat from './components/main/AIChat';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const firebaseConfig = {
   apiKey: "AIzaSyASeI6SIWU4uBdQ8SYDdpoqR9yqwiuiFJM",
@@ -24,68 +30,68 @@ const firebaseConfig = {
   measurementId: "G-WL7Y47YWHS"
 };
 
-const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app();
-
-const db = app.firestore;
-const auth = app.auth;
-const storage = firebase.storage;
-
-export { auth, db, storage };
-
-
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import LandingScreen from './components/auth/Landing';
-import RegisterScreen from './components/auth/Register';
-import LoginScreen from './components/auth/Login';
-import MainScreen from './components/Main';
-import AddScreen from './components/main/Add';
-import SaveScreen from './components/main/Save';
-import AIChat from './components/main/AIChat';
-
-
 const Stack = createStackNavigator();
-
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       loaded: false,
-    }
+      loggedIn: false,
+      isConnected: true,
+    };
   }
-  componentDidMount(){
+
+  componentDidMount() {
+    this.checkConnection();
     firebase.auth().onAuthStateChanged((user) => {
       if (!user) {
         this.setState({
           loggedIn: false,
           loaded: true,
-        })
+        });
       } else {
         this.setState({
           loggedIn: true,
           loaded: true,
-        })
+        });
       }
-    })
+    });
   }
+
+  checkConnection = () => {
+    NetInfo.addEventListener(state => {
+      this.setState({ isConnected: state.isConnected });
+      if (!state.isConnected) {
+        Alert.alert('Bağlantı Hatası', 'Uygulama internet bağlantısı gerektirir.');
+      }
+    });
+  };
+
   render() {
-    const { loggedIn, loaded } = this.state;
+    const { loggedIn, loaded, isConnected } = this.state;
+
     if (!loaded) {
       return (
-        <View style={{ flex: 1, justifyContent: 'center'}}>
+        <View style={styles.container}>
           <Text>Loading</Text>
         </View>
-      )
+      );
     }
-    if(!loggedIn){
+
+    if (!isConnected) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.offlineText}>İnternet bağlantısı yok. Lütfen bağlantınızı kontrol edin.</Text>
+        </View>
+      );
+    }
+
+    if (!loggedIn) {
       return (
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Landing">
@@ -94,28 +100,35 @@ export class App extends Component {
             <Stack.Screen name="Login" component={LoginScreen} />
           </Stack.Navigator>
         </NavigationContainer>
-      )
+      );
     }
+
     return (
-      <Provider store={ store }>
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
-            <Stack.Screen name="Main" component={MainScreen}  />
+            <Stack.Screen name="Main" component={MainScreen} />
             <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
             <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
-            <Stack.Screen name="AIChat" component={AIChat} navigaton={this.props.navigaton} />
-          </Stack.Navigator> 
+            <Stack.Screen name="AIChat" component={AIChat} navigation={this.props.navigation} />
+          </Stack.Navigator>
         </NavigationContainer>
       </Provider>
-
-        
-    )
+    );
   }
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  offlineText: {
+    fontSize: 18,
+    color: 'red',
+  },
+});
 
-
-
-
-
+export default App;
